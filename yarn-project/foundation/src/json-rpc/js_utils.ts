da43d1f2f2fe00219bb0805f9b60bddd5c73bc80
ludamad <adam.domurad@gmail.com>
2023-08-16 18:46:57 -0400

    Get cgam compiling again

--- yarn-project/foundation/src/json-rpc/js_utils.ts
+++ yarn-project/foundation/src/json-rpc/js_utils.ts
@@ -8,3 +8,15 @@ export function assert(x: any, err: string): asserts x {
     throw new Error(err);
   }
 }
+
+/**
+ * Checks if an object has a specific property as its own (not inherited).
+ *
+ * @param obj - The object to check.
+ * @param prop - The property name to check for.
+ * @returns True if the object has the property, false otherwise.
+ */
+export function hasOwnProperty(obj: object, prop: string) {
+  // Ensure that obj is an object and has the hasOwnProperty method.
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
