edacaf4d46b1210f0799102d4346c940af60207d
ludamad <adam.domurad@gmail.com>
2023-08-23 14:09:39 -0400

    Updates to cgam

--- yarn-project/end-to-end/src/e2e_c_gam.test.ts
+++ yarn-project/end-to-end/src/e2e_c_gam.test.ts
@@ -56,7 +56,7 @@ describe('e2e_c_gam_contract', () => {
     return cardData;
   };
 
-  it.skip('should call buy_pack and see notes', async () => {
+  it('should call buy_pack and see notes', async () => {
     const deployedContract = await deployContract();
     const cardData = await buyPackAndGetData(deployedContract, owner, logger);
     // Test that we have received the expected card data
@@ -78,82 +78,4 @@ describe('e2e_c_gam_contract', () => {
     );
     await tx.send({ origin: owner.address }).wait();
   }, 30_000);
-
-  // /**
-  //  * Milestone 1.3.
-  //  * https://hackmd.io/AG5rb9DyTRu3y7mBptWauA
-  //  */
-  // it('1.3 should deploy zk token contract with initial token minted to the account', async () => {
-  //   const initialBalance = 987n;
-  //   const owner = await aztecRpcServer.getAccountPublicKey(accounts[0]);
-  //   await deployContract(initialBalance, pointToPublicKey(owner));
-  //   await expectBalance(accounts[0], initialBalance);
-  //   await expectBalance(accounts[1], 0n);
-
-  //   await expectsNumOfEncryptedLogsInTheLastBlockToBe(1);
-  //   await expectUnencryptedLogsFromLastBlockToBe(['Balance set in constructor']);
-  // }, 30_000);
-
-  // /**
-  //  * Milestone 1.4.
-  //  */
-  // it('1.4 should call mint and increase balance', async () => {
-  //   const mintAmount = 65n;
-
-  //   const [owner] = accounts;
-  //   const ownerPublicKey = pointToPublicKey(await aztecRpcServer.getAccountPublicKey(owner));
-
-  //   const deployedContract = await deployContract(0n, ownerPublicKey);
-  //   await expectBalance(owner, 0n);
-
-  //   await expectsNumOfEncryptedLogsInTheLastBlockToBe(1);
-  //   await expectUnencryptedLogsFromLastBlockToBe(['Balance set in constructor']);
-
-  //   const tx = deployedContract.methods.mint(mintAmount, ownerPublicKey).send({ from: owner });
-
-  //   await tx.isMined(0, 0.1);
-  //   const receipt = await tx.getReceipt();
-
-  //   expect(receipt.status).toBe(TxStatus.MINED);
-  //   await expectBalance(owner, mintAmount);
-
-  //   await expectsNumOfEncryptedLogsInTheLastBlockToBe(1);
-  //   await expectUnencryptedLogsFromLastBlockToBe(['Coins minted']);
-  // }, 60_000);
-
-  // /**
-  //  * Milestone 1.5.
-  //  */
-  // it('1.5 should call transfer and increase balance of another account', async () => {
-  //   const initialBalance = 987n;
-  //   const transferAmount = 654n;
-  //   const [owner, receiver] = accounts;
-
-  //   await deployContract(initialBalance, pointToPublicKey(await aztecRpcServer.getAccountPublicKey(owner)));
-
-  //   await expectBalance(owner, initialBalance);
-  //   await expectBalance(receiver, 0n);
-
-  //   await expectsNumOfEncryptedLogsInTheLastBlockToBe(1);
-  //   await expectUnencryptedLogsFromLastBlockToBe(['Balance set in constructor']);
-
-  //   const tx = contract.methods
-  //     .transfer(
-  //       transferAmount,
-  //       pointToPublicKey(await aztecRpcServer.getAccountPublicKey(owner)),
-  //       pointToPublicKey(await aztecRpcServer.getAccountPublicKey(receiver)),
-  //     )
-  //     .send({ from: accounts[0] });
-
-  //   await tx.isMined(0, 0.1);
-  //   const receipt = await tx.getReceipt();
-
-  //   expect(receipt.status).toBe(TxStatus.MINED);
-
-  //   await expectBalance(owner, initialBalance - transferAmount);
-  //   await expectBalance(receiver, transferAmount);
-
-  //   await expectsNumOfEncryptedLogsInTheLastBlockToBe(2);
-  //   await expectUnencryptedLogsFromLastBlockToBe(['Coins transferred']);
-  // }, 60_000);
 });
