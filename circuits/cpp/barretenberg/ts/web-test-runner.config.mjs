6c9f8ad6235609f8c6b5fce7163222261b638c64
Koby <kobyhall@proton.me>
2023-08-26 11:38:54 +0200

    chore: Add webkit and ff to tested browsers

--- circuits/cpp/barretenberg/ts/web-test-runner.config.mjs
+++ circuits/cpp/barretenberg/ts/web-test-runner.config.mjs
@@ -7,14 +7,19 @@ import rollupCommonjs from '@rollup/plugin-commonjs';
 const commonjs = fromRollup(rollupCommonjs);
 
 async function add_headers(ctx, next) {
-  ctx.set('Cross-Origin-Opener-Policy', "same-origin");
-  ctx.set('Cross-Origin-Embedder-Policy', "require-corp");  
+  ctx.set('Cross-Origin-Opener-Policy', 'same-origin');
+  ctx.set('Cross-Origin-Embedder-Policy', 'require-corp');
   await next(ctx);
 }
 
 export default {
   middlewares: [add_headers],
-  browsers: [playwrightLauncher({ product: 'chromium' })],
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    // WebKit currently failing in comlink with "The object can not be cloned"
+    //playwrightLauncher({ product: 'webkit' }),
+    playwrightLauncher({ product: 'firefox' }),
+  ],
   nodeResolve: { browser: true },
   plugins: [
     commonjs({
