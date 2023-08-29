58440621e2a2b76feb83830c62e8e6cf2cca5852
Koby <kobyhall@proton.me>
2023-08-29 16:43:33 +0200

    chore(test): explored with webdriver

--- circuits/cpp/barretenberg/ts/web-test-runner.config.mjs
+++ circuits/cpp/barretenberg/ts/web-test-runner.config.mjs
@@ -1,6 +1,7 @@
 // import { fileURLToPath } from 'url';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { playwrightLauncher } from '@web/test-runner-playwright';
+import { webdriverLauncher } from '@web/test-runner-webdriver';
 import { fromRollup } from '@web/dev-server-rollup';
 import rollupCommonjs from '@rollup/plugin-commonjs';
 
@@ -18,16 +19,52 @@ export default {
   // We need to set these heders for workers to work
   middlewares: [add_headers],
   browsers: [
+    // (~103.9s) for `double_verify_proof`
+    // All below times refer `double_verify_proof` on MBP 8 cores i9
     playwrightLauncher({ product: 'chromium' }),
-    // WebKit currently failing in comlink with "The object can not be cloned"
+    
+    // WebKit currently failing with `Error: page.goto: Page crashed`
+    // Works ok on regular Safari
     // playwrightLauncher({ product: 'webkit' }),
     
     // firefox seems to suffer from known bug causing
     // Error: page.goto: Navigation to "about:blank" is interrupted by another navigation to "about:blank"
     // more https://github.com/microsoft/playwright/issues/6202
     // bu test shall pass
+    // besides that, firefox seems to be extremally slow (10m) vs (<2m) in latest firefox when manual testing is launched
     // playwrightLauncher({ product: 'firefox' }),
+
+
+    // Below options would need "some" system permissions (macOS specificallly)
+    // To control applications
+
+    // Completes in ~2m
+    // webdriverLauncher({
+    //   automationProtocol: 'webdriver',
+    //   // path: '/wd/hub/',
+    //   capabilities: {
+    //     browserName: 'firefox',
+    //     'moz:firefoxOptions': {
+    //       args: ['-headless'],
+    //     },
+    //   },
+    // }),
+
+    // Needs `Allow Remote Automation` enabled from `Develop` menu
+    // Completes in ~ 2m
+    // webdriverLauncher({
+    //   automationProtocol: 'webdriver',
+    //   // path: '/wd/hub/',
+    //   capabilities: {
+    //     browserName: 'safari',
+    //     // 'moz:firefoxOptions': {
+    //     //   args: ['-headless'],
+    //     // },
+    //   },
+    // }),
+
   ],
+  testsFinishTimeout: 420000,
   nodeResolve: { browser: true },
   plugins: [
     commonjs({
@@ -40,12 +77,12 @@ export default {
       // tsconfig: fileURLToPath(new URL('./tsconfig.browser.json', import.meta.url)),
     }),
   ],
-  files: ['test/browser/sanity/**/*.browser.test.ts'],
+  files: ['test/browser/extended/**/*.browser.test.ts'],
 
   testFramework: {
     config: {
       ui: 'bdd',
-      timeout: 400000,
+      timeout: 420000,
     },
   },
 
